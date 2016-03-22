456be4dfe7614950ab32e400cda999561f34f72c
Ronaldo Felipe <ronaldolocalhost@gmail.com>
2016-03-21 22:40:56 -0300

    Flow between scenes created and multiple musics support.

---
+++ GlitchGarden/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public float autoLoadNextLevelAfter;
+
+	void Start () {
+        if (autoLoadNextLevelAfter>0)
+		    Invoke ("LoadNextLevel", autoLoadNextLevelAfter);
+	}
+
+    public void LoadLevel(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+    public void LoadScene(Scene scene)
+    {
+        SceneManager.LoadScene(scene.name);
+    }
+
+    public void QuitRequest(){
+		Debug.Log ("Quit requested");
+		Application.Quit ();
+	}
+	
+	public void LoadNextLevel() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+}
---
+++ GlitchGarden/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour
+{
+
+    private static MusicPlayer instance;
+
+    public AudioClip[] musicChangeArray;
+
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+        else {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnLevelWasLoaded(int level)
+    {
+        audioSource.Stop();
+        if (level > 0 && level <= musicChangeArray.Length && musicChangeArray[level]!=null) {
+            audioSource.clip = (musicChangeArray[level]);
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+    }
+}
