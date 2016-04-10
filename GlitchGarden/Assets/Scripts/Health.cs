764867718cf96a70b4eeda3197501eac2faab99a
Ronaldo Felipe <ronaldolocalhost@gmail.com>
2016-04-10 12:32:30 -0300

    Every character has a health bar, now.

--- GlitchGarden/Assets/Scripts/Health.cs
+++ GlitchGarden/Assets/Scripts/Health.cs
@@ -1,13 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Health : MonoBehaviour {
 
 	public float healthPoints;
 
+	private float maxHP;
+	private Slider hpSlider;
+
 	// Use this for initialization
 	void Start () {
-	
+		maxHP = healthPoints;
+		hpSlider = GetComponentInChildren<Slider>();
+		if (!hpSlider) Debug.LogWarning ("Object "+name+ "doesn't have a Slider UI.");
 	}
 	
 	// Update is called once per frame
@@ -17,6 +23,7 @@ public class Health : MonoBehaviour {
 
 	public bool LoseHP(float damage){
 		healthPoints -= damage;
+		if (hpSlider) hpSlider.value = healthPoints/maxHP;
 		if(healthPoints<=0){
 			//in case of dead animation
 			DestroyActor();
