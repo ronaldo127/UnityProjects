993b2541e901056d47da413f78df71b8e886c252
Ronaldo Felipe <ronaldolocalhost@gmail.com>
2016-04-12 20:21:58 -0300

    BowlMaster project initial upload.

---
+++ Bowlmaster/Assets/Scripts/Ball.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Ball : MonoBehaviour {
+
+	public float initialVelocity = 1f;
+	private AudioSource rollingSound;
+
+	private Rigidbody rigidBody;
+	// Use this for initialization
+	void Start () {
+		SetupVariables ();
+		Launch ();
+	}
+
+	void SetupVariables ()
+	{
+		rigidBody = GetComponent<Rigidbody> ();
+		rollingSound = GetComponent<AudioSource> ();
+	}
+
+	public void Launch ()
+	{
+		rigidBody.velocity = ( Vector3.forward * initialVelocity);
+		rollingSound.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Bowlmaster/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+	public float stopZ=1829;
+
+	private Vector3 offset;
+	private Ball ball;
+
+	// Use this for initialization
+	void Start () {
+		ball = GameObject.FindObjectOfType<Ball>();
+		offset = this.transform.position-ball.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.transform.position.z < stopZ)
+			this.transform.position = ball.transform.position + offset;
+	}
+}
