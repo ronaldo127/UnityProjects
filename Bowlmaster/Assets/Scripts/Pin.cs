9a12e879e8cdc2fd9337abadcb225d0eb3f4cb4b
Ronaldo Felipe <ronaldolocalhost@gmail.com>
2016-04-16 12:26:31 -0300

    It show how many pins are standing.

---
+++ Bowlmaster/Assets/Scripts/Pin.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pin : MonoBehaviour {
+
+	public float stadingThreshold=45;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+
+	public bool IsStanding ()
+	{
+		Vector3 angles = transform.rotation.eulerAngles;
+		float xAngle = angles.x;
+		float zAngle = angles.z;
+		return (Mathf.Abs(xAngle-360.0f)<stadingThreshold || xAngle<stadingThreshold) && 
+			(zAngle<stadingThreshold || Mathf.Abs(zAngle-360.0f)<stadingThreshold);
+	}
+}
---
+++ Bowlmaster/Assets/Scripts/PinSetter.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PinSetter : MonoBehaviour {
+
+	public Text standingText;
+
+	private bool ballEnteredBox = false;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		standingText.text = CountStanding().ToString();
+	}
+
+	public int CountStanding ()
+	{
+		Pin[] pins = GameObject.FindObjectsOfType<Pin>();
+		int count = 0;
+		foreach (Pin pin in pins) {
+			if (pin.IsStanding())
+				count++;
+		}
+		return count;
+	}
+
+	void OnTriggerEnter (Collider collider)
+	{
+		Ball ball = collider.GetComponent<Ball>();
+		if (ball){
+			standingText.color = Color.red;
+			ballEnteredBox = true;
+		}
+	}
+	void OnTriggerExit (Collider collider)
+	{
+		Pin pin = collider.GetComponentInParent<Pin>();
+		if (pin) {
+			Destroy(pin);
+		}
+	}
+}
