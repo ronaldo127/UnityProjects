4aa8750c3f212102e70da582286a6fee3e58a0aa
Ronaldo Felipe <ronaldolocalhost@gmail.com>
2016-04-17 15:30:17 -0300

    PlayBox is avoiding the ball falls forever.

--- Bowlmaster/Assets/Scripts/PinSetter.cs
+++ Bowlmaster/Assets/Scripts/PinSetter.cs
@@ -4,27 +4,29 @@ using System.Collections;
 
 public class PinSetter : MonoBehaviour {
 
-	public int lastStandingCount = -1;
 	public Text standingText;
 	public float distanceToRaise = 40.0f;
 	public GameObject pinSet;
 
-	private Ball ball;
+	private int lastStandingCount = -1;
+	private bool ballLeftBox = false;
 	private float lastChangeTime;
-	private bool ballEnteredBox = false;
+
+	private Ball ball;
 	private Animator animator;
+	private ActionMaster player1;
 
 	// Use this for initialization
 	void Start () {
 		ball = GameObject.FindObjectOfType<Ball>();
 		animator = GetComponent<Animator>();
+		player1 = new ActionMaster();
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		standingText.text = CountStanding ().ToString ();
-		if (ballEnteredBox) {
+		if (ballLeftBox) {
 			UpdateStandingCount();
 			UpdatePinSettled();
 		}
@@ -48,13 +50,13 @@ public class PinSetter : MonoBehaviour {
 
 	public void RenewPins ()
 	{
-		print("RenewPins!");
 		Instantiate(pinSet, pinSet.transform.position, Quaternion.identity);
 	}
 
 	void UpdateStandingCount ()
 	{
 		int currentStanding = CountStanding ();
+		standingText.text = currentStanding.ToString();
 		if (lastStandingCount != currentStanding) {
 			lastStandingCount = currentStanding;
 			lastChangeTime = Time.time;
@@ -64,15 +66,33 @@ public class PinSetter : MonoBehaviour {
 	void UpdatePinSettled ()
 	{
 		if (Time.time - lastChangeTime >= 3.0f) {
-			ball.Reset();
-			lastStandingCount=-1;
-			ballEnteredBox=false;
+			ActionMaster.Action action = player1.Bowl (CountFallen ());
+			ball.Reset ();
+			lastStandingCount = -1;
+			ballLeftBox = false;
 
 			float r = 0x4c;
 			float g = 0xaf;
 			float b = 0x50;
 			float max = 0xff;
-			standingText.color = new Color(r/max, g/max, b/max);
+			standingText.color = new Color (r / max, g / max, b / max);
+			switch (action) {
+			case ActionMaster.Action.RESET:
+				{
+					Reset();
+					break;
+				}
+			case ActionMaster.Action.TIDY:
+				{
+					Tidy();
+					break;
+				}
+			case ActionMaster.Action.END_TURN:
+				{
+					Reset();
+					break;
+				}
+			}
 		}
 	}
 
@@ -92,25 +112,24 @@ public class PinSetter : MonoBehaviour {
 		return 10-CountStanding();
 	}
 
-	void OnTriggerEnter (Collider collider)
-	{
-		Ball ball = collider.GetComponent<Ball>();
-		if (ball){
-			float r = 0xf4;
-			float g = 0x43;
-			float b = 0x36;
-			float max = 0xff;
-			standingText.color = new Color(r/max, g/max, b/max);
-			ballEnteredBox = true;
-		}
-	}
 
 	public void Reset ()
 	{
+		standingText.color = Color.black;
 		animator.SetTrigger("resetTrigger");
 	}
 	public void Tidy()
 	{
 		animator.SetTrigger("tidyTrigger");
+	}
+
+	public void BallLeftBox ()
+	{
+		float r = 0xf4;
+		float g = 0x43;
+		float b = 0x36;
+		float max = 0xff;
+		standingText.color = new Color(r/max, g/max, b/max);
+		ballLeftBox = true;
 	}
 }
---
+++ Bowlmaster/Assets/Scripts/PlayBox.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayBox : MonoBehaviour {
+
+	private PinSetter pinSetter;
+
+	// Use this for initialization
+	void Start () {
+		pinSetter = GameObject.FindObjectOfType<PinSetter>();
+	}
+
+	void OnTriggerExit (Collider col)
+	{
+		if (col.GetComponent<Ball> ()) {
+			pinSetter.BallLeftBox();
+		}
+	}
+}
