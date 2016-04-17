73ff6f366373c009259804fa77caad83024cc78e
Ronaldo Felipe <ronaldolocalhost@gmail.com>
2016-04-17 13:38:26 -0300

    More Testing.

--- Bowlmaster/Assets/Scripts/ActionMaster.cs
+++ Bowlmaster/Assets/Scripts/ActionMaster.cs
@@ -17,34 +17,53 @@ public class ActionMaster {
 			throw new UnityException ("Invalid pins count");
 
 		bowls [bowl] = pins;
-		if (bowl==21) return Action.END_GAME;
-		if (bowl == 20) {
-			if (Has21Frame()) {
+		if (bowl == 21)//on bowl 21
+			return Action.END_GAME;
+
+		//special cases
+		if (bowl == 20) { //on bowl 20
+			if (Has21Frame ()) {
+				if (pins == 10 || IsSpare ()) {//if is a strike or a spare
+					bowl++;	
+					return Action.RESET;
+				}
+				//on strike not followed by a strike or spare
 				bowl++;
-				return Action.RESET;
+				return Action.TIDY;
 			}
 			return Action.END_GAME;
 		}
 
-		if (pins == 10) {
+		if (pins == 10) {//if is a trike
 			if (bowl > 18) {
 				bowl++;
 				return Action.RESET;
+			}
+			if (IsSpare ()) {
+				bowl++;
 			} else {
 				bowl += 2;
 			}
 			return Action.END_TURN;
-		}
-		if (bowl % 2 == 1) {
-			bowl++;
-			return Action.TIDY;
-		} else {
-			bowl++;
-			return Action.END_TURN;
+		} 
+		else
+		{
+			if (bowl % 2 == 1) {//if is first launch and not strike
+				bowl++;
+				return Action.TIDY;
+			} else {//if is second launch and not strike
+				bowl++;
+				return Action.END_TURN;
+			}
 		}
 	}
 
 	private bool Has21Frame(){
-		return bowls[bowl]+bowls[bowl-1] == 10||bowls[bowl-1]==10;
+		return IsSpare()||bowls[bowl-1]==10;
+	}
+
+	private bool IsSpare ()
+	{
+		return (bowl%2==0&&bowl<19)||(bowls[bowl]+bowls[bowl-1] == 10&&bowls[bowl]!=10&&bowls[bowl-1]!=10);
 	}
 }
