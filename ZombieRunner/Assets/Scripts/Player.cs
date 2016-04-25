432a69d40e05964fa54ffc0cef9ccbfe2380107d
Ronaldo Felipe <ronaldolocalhost@gmail.com>
2016-04-24 22:23:39 -0300

    Some adjustments on AI and player can respawn randomly

---
+++ ZombieRunner/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public bool shouldRespawn = false;
+
+	private GameObject spawnsHolder;
+
+	// Use this for initialization
+	void Start () {
+		this.spawnsHolder = GameObject.Find("PlayerSpawnPoints");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (shouldRespawn) {
+			this.ReSapawn();
+			shouldRespawn = false;
+		}
+	}
+
+
+	public void ReSapawn ()
+	{
+		int spawns = spawnsHolder.transform.childCount;
+		Transform spawn = this.spawnsHolder.transform.GetChild(Random.Range(0,spawns));
+		this.transform.position = spawn.position;
+	}
+}
